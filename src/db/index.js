55499298a56b8fb848589b96e63d5e5464e310a0
manishb70 <mb8882568627@gmail.com>
2024-07-21 22:12:29 +0530

    db key updatred

--- src/db/index.js
+++ src/db/index.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
     try {
 
         // const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URI}/${DB_NAME}`)
-        const connectionInstance = await mongoose.connect("mongodb+srv://manishb79:db123@manish.x5h9k0c.mongodb.net/youtube")
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB conneted !! DB HOST ${connectionInstance.connection.host} `);
     } catch (error) {
         console.error("Mongo db connection error ", error)
