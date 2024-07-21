33ba1497a7a0d0d9ed77e6006baa8e542782ecda
manishb70 <mb8882568627@gmail.com>
2024-07-21 22:04:41 +0530

    jfioefj

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,8 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
 
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URI}/${DB_NAME}`)
+        // const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect("mongodb+srv://manishb79:db123@manish.x5h9k0c.mongodb.net/youtube")
         console.log(`\n MongoDB conneted !! DB HOST ${connectionInstance.connection.host} `);
     } catch (error) {
         console.error("Mongo db connection error ", error)
